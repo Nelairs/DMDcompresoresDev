36d1a49bd87c49ff389fb95d607a4374d4344daa
Nico1409 <nicolas140902@gmail.com>
2022-05-11 13:54:10 -0300

    segundo cambio

--- practica-post/api2.js
+++ practica-post/api2.js
@@ -1,14 +1,14 @@
 let carta =''
-
 axios.get('http://175.10.0.166:8080/list')
 .then((res)=>{
     console.log(res.data)
     res.data.forEach(data=> {
-        console.log(data.title)
         carta += `
-        <div class="">
+        <div class=" border-2 border-gray-600 rounded-xl w-60 flex flex-col items-center">
        <h1 class="text-sky-400">${data.title}</h1>
+       <p class="text-xs">${data.description}</p>
        <img class="w-40 h-40" src="${data.image}">
+       <h2 class="">Price: ${data.price}</h2>
         </div>
 
 
